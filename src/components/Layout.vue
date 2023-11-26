ee1bc93c6263eba096c6c736634d17d89efbc707
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-11-25 22:44:20 -0300

    feat: estilos globais e estrutura do app

---
+++ src/components/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  height: 100vh;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: 240px 1fr;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+  <aside class="sidebar">
+    <!-- sidebar content -->
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+.sidebar {
+  background-color: var(--sidebar);
+  box-shadow: rgba(255, 255, 255, 0.05) -1px 0px 0px 0px inset;
+}
+</style>
