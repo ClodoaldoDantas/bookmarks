b7ccb8f66603612437fa39c3b3ce21f020575f69
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-11-25 23:21:12 -0300

    feat: menu de ações (interface)

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,25 @@
+<script setup lang="ts">
+import { PlusCircle, LogOut } from 'lucide-vue-next'
+</script>
+
 <template>
   <aside class="sidebar">
     <div class="profile">
       <img src="https://github.com/clodoaldodantas.png" alt="" />
       <span>Clodoaldo Dantas</span>
     </div>
+
+    <nav class="navigation-actions">
+      <button type="button">
+        <PlusCircle :size="18" />
+        <span>Nova Página</span>
+      </button>
+
+      <button type="button">
+        <LogOut :size="18" />
+        <span>Sair da Conta</span>
+      </button>
+    </nav>
   </aside>
 </template>
 
@@ -28,6 +44,32 @@
 
   span {
     font-size: 0.875rem;
+  }
+}
+
+.navigation-actions {
+  margin-top: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.625rem;
+
+  button {
+    background: none;
+    border: none;
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    font-size: 0.875rem;
+    color: var(--text-secondary);
+    transition: color 200ms;
+
+    &:hover {
+      color: var(--text-primary);
+    }
   }
 }
 </style>
