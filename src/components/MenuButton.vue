46c84f95b4f464c65dd7fb5b1297451734267adf
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-11-26 18:19:44 -0300

    refactor: criando mixin para o estilo dos links

--- src/components/MenuButton.vue
+++ src/components/MenuButton.vue
@@ -5,25 +5,8 @@
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/mixins';
 .menu-button {
-  background: transparent;
-  border: none;
-  border-radius: 5px;
-  width: 95%;
-
-  padding: 0.5rem 0.75rem;
-  margin: 0 auto;
-
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-
-  font-size: 0.875rem;
-  color: var(--text-secondary);
-  transition: color 200ms;
-
-  &:hover {
-    background-color: rgba(255, 255, 255, 0.055);
-  }
+  @include mixins.action-button;
 }
 </style>
